cc06e2d4a8b6ddf756c1ce152701fe54e7e56229
andreykozyrev <akozyrev222@gmail.com>
2024-09-25 18:37:59 +0300

    add new buttons

--- client/src/components/Toolbar.jsx
+++ client/src/components/Toolbar.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import '../styles/toolbar.scss'
+import {FaEraser, FaPaintBrush, FaRedo, FaRegCircle, FaSave, FaUndo} from "react-icons/fa";
+import {MdOutlineRectangle, MdRectangle} from "react-icons/md";
+import {IoPencilOutline} from "react-icons/io5";
 
 const Toolbar = () => {
     return (
         <div className='toolbar'>
-            
+            <button className='toolbar__btn'>
+                <FaPaintBrush size={25}/>
+            </button>
+            <button className='toolbar__btn'>
+                <MdRectangle size={25}/>
+            </button>
+            <button className='toolbar__btn'>
+                <FaRegCircle size={25}/>
+            </button>
+            <button className='toolbar__btn'>
+                <FaEraser size={25}/>
+            </button>
+            <button className='toolbar__btn '>
+                <IoPencilOutline
+                    size={25}/>
+            </button>
+            <input className='toolbar__btn ' type='color'/>
+            <button className='toolbar__btn left'>
+                <FaUndo
+                    size={25}/>
+            </button>
+            <button className='toolbar__btn '>
+                <FaRedo
+                    size={25}/>
+            </button>
+            <button className='toolbar__btn right'>
+                <FaSave
+                    size={25}/>
+            </button>
         </div>
     );
 };
